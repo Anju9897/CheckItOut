cc8803913173d921dfc5ca34e7f087cdebc7da84
Juan Ramos <anju9897@gmail.com>
2019-06-12 16:52:31 -0600

    correcciones

--- js/login.js
+++ js/login.js
@@ -9,16 +9,7 @@ function muestra_oculta(id,id2){
     }
 }
 
-
-function m_o(id){
-    if (document.getElementById){ //se obtiene el id
-        var el = document.getElementById(id); //se define la variable "el" igual a nuestro div
-        el.style.display = (el.style.display == 'none') ? 'flex' : 'none';
-    //damos un atributo display:none que oculta el div
-    }
-}
-
 window.onload = function(){/*hace que se cargue la función lo que predetermina que div estará oculto hasta llamar a la función nuevamente*/
     muestra_oculta('','');/* "contenido_a_mostrar" es el nombre que le dimos al DIV */
-    m_o('');
+
 }
